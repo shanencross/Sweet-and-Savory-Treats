c216c2f9d1da150ed680eeac79638053d38fa966
Shanen Cross <shanencross@yahoo.com>
2021-06-20 07:45:36 -0700

    Fill out flavor controller CRUD actions

--- SweetAndSavoryTreats/Controllers/FlavorsController.cs
+++ SweetAndSavoryTreats/Controllers/FlavorsController.cs
@@ -16,5 +16,72 @@ namespace SweetAndSavoryTreats.Controllers
     {
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Flavor> flavors = _db.Flavors.ToList();
+      return View(flavors);
+    }
+
+    [Authorize]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+        .Include(flavor => flavor.FlavorTreats)
+        .ThenInclude(flavorTreat => flavorTreat.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [Authorize]
+    public ActionResult Edit(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [Authorize]
+    public ActionResult Delete(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [Authorize]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      if (id != 0)
+      {
+        _db.Flavors.Remove(thisFlavor);
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
